b756aed666ef582553f261d1c20e61f2b3e53193
DerSkythe <31771569+derskythe@users.noreply.github.com>
2024-12-24 15:33:50 +0400

    Ci cd (#2)
    * ci: update workflows and add clean-up process
    
    * Replace `pull_request` trigger with `push` for build workflow, limiting to `master` and `cicd` branches.
    * Add a new `clean-up.yaml` workflow to handle canceled and failed runs.
    * Update IDE VCS settings to disable branch protection synchronization.
    
    * refactor: update workflows to use standardized firmware naming
    
    * Replaced FILE_NAME with FIRMWARE_NAME in build workflows for clarity.
    * Enhanced PowerShell script to zip firmware files and handle upload and tag creation errors.
    * Added detailed file size formatting and improved release structure.
    
    * ci(build): add SHA output to workflow
    
    * Added SHA output to capture the current commit hash in the build step.
    * Updated dependent steps to include and utilize the SHA value.
    * Modified release script to pass SHA as an argument for consistency.
    
    * chore(ci): remove json validation from build workflow
    
    * Removed redundant json validation step for build process.
    * Simplifies the workflow by eliminating unnecessary checks.
    * Ensures less potential maintenance overhead in future updates.
    
    * chore(workflows): update permissions and token usage
    
    * Added `permissions` field with `contents: read` in `yaml-lint` workflow.
    * Replaced `GITHUB_TOKEN` with `GH_TOKEN` in `build` workflow for uniform token usage.
    * Improves alignment with GitHub best practices for security and configuration.
    
    * fix(workflow): remove redundant run_attempt from version tag
    
    * Removed `${{ github.run_attempt }}` from version calculation in build workflow.
    * Simplified versioning to ensure consistent and predictable output.
    
    * ci: update workflows and scripts for improved release handling
    
    * Set custom tag variable in PowerShell script for consistent releases.
    * Replace workflow output file handling with environment vars for clarity.
    * Simplify and standardize release-related API calls with the new tag variable.
    * Add timeout to build job for better task management.
    
    * fix(create-or-update-release): add zero-length file check
    
    Improve script robustness by checking for zero-length files.
    
    * Added a condition to detect and handle cases where input file size is zero.
    * Removed redundant directory creation and file copying commands.

--- .github/create-or-update-release.ps1
+++ .github/create-or-update-release.ps1
@@ -8,12 +8,55 @@ $ErrorActionPreference = "Stop"        #
 ########################################
 
 [string]$InputDir = $args[0]
-[string]$ReleaseVersion = $args[1]
-[string]$Repo = $args[2]
-[string]$Branch = $args[3]
+[string]$AppName = $args[1]
+[string]$ReleaseVersion = $args[2]
+[string]$Repo = $args[3]
+[string]$Branch = $args[4]
+[string]$Sha = $args[5]
 [string]$CurrentLocation = (Get-Location)
+[string]$Tag = "$ReleaseVersion"
 
-$Files = @(Get-ChildItem -Path "$InputDir/*" -File -Include "*.tgz", "*.zip" -ErrorAction SilentlyContinue)
+function Format-Bytes
+{
+    param(
+        [int]$number
+    )
+    if(0 -eq $number) {
+        Write-Error ('::error title=File has zero lenght!::Error')
+        exit 2
+    }
+    $sizes = 'KB', 'MB', 'GB', 'TB', 'PB'
+    for ($x = 0; $x -lt $sizes.count; $x++) {
+        if ($number -lt [int64]"1$( $sizes[$x] )")
+        {
+            if ($x -eq 0)
+            {
+                return "$number B"
+            }
+            else
+            {
+                $formattedNumber = $number / [int64]"1$( $sizes[$x - 1] )"
+                $formattedNumber = "{0:N2}" -f $formattedNumber
+                return "$formattedNumber $( $sizes[$x - 1] )"
+            }
+        }
+    }
+}
+
+Set-Location $InputDir
+[string]$ZipName = ('{0}.zip' -f $AppName)
+
+zip -r -qq $ZipName "./$AppName"
+
+if (!(Test-Path -Path $ZipName -PathType Leaf))
+{
+    Write-Error '::error title=Files not found::Cannot find files in location'
+    exit 1
+}
+
+[string]$ZipSize = Format-Bytes (Get-Item -Path $ZipName).Length
+
+$Files = @(Get-ChildItem -Path "./*" -File -Include "*.zip" -ErrorAction SilentlyContinue)
 if ($Files.Count -eq 0)
 {
     Write-Error ('::error title=Files not found::Cannot READ files in location: {0}' -f $InputDir)
@@ -29,13 +72,35 @@ if ($LASTEXITCODE -gt 1)
 }
 
 Write-Host "Start upload"
-Set-Location $InputDir
-$Files | ForEach-Object {
-    Write-Host ('Uploading {0}' -f $_.Name)
-    gh release upload $ReleaseVersion $_.Name -R $Repo --clobber
+
+[string]$Mime = "Accept: application/vnd.github+json"
+[string]$Api = "X-GitHub-Api-Version: 2022-11-28"
+$Json = ((gh api -H $Mime -H $Api /repos/$Repo/releases) | ConvertFrom-Json)
+$TagExists = ($null -ne ($Json.GetEnumerator() | Where-Object { $_.tag_name -eq "$Tag" }))
+if ($false -eq $TagExists)
+{
+    gh api --method POST -H $Mime -H $Api /repos/$Repo/releases -f tag_name="$Tag" -f target_commitish="$Sha" -f name="$Tag" -F draft=false -F prerelease=false -F generate_release_notes=true
+
+    if ($LASTEXITCODE -gt 1)
+    {
+        Write-Error ('::error title=Failed to create new tag::Tag: {0}, Repo: {1}, ErrorCode: {2}' -f $ReleaseVersion, $Repo, $LASTEXITCODE)
+        exit 255
+    }
+}
+gh release upload "$Tag" "$ZipName#$ZipName $ZipSize" --clobber -R $Repo
+
+if ($LASTEXITCODE -gt 1)
+{
+    Write-Error ('::error title=Failed to upload::Tag: {0}, Repo: {1}, ErrorCode: {2}' -f $ReleaseVersion, $Repo, $LASTEXITCODE)
+    exit 255
 }
 
-Write-Host "Release edit"
-gh release edit $ReleaseVersion -R $Repo --latest --target $Branch
+gh release edit "$Tag" --draft=false -R $Repo
+
+if ($LASTEXITCODE -gt 1)
+{
+    Write-Error ('::error title=Failed to edit release::Tag: {0}, Repo: {1}, ErrorCode: {2}' -f $ReleaseVersion, $Repo, $LASTEXITCODE)
+    exit 255
+}
 
 Set-Location $CurrentLocation
