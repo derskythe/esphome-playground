ed6443034290fafec64c605fc23146ca1df9f1b6
DerSkythe <31771569+derskythe@users.noreply.github.com>
2025-09-03 20:14:27 +0400

    Add firewall rules for Docker and WSL outbound traffic
    Signed-off-by: DerSkythe <31771569+derskythe@users.noreply.github.com>

--- script.ps1
+++ script.ps1
@@ -4,3 +4,12 @@ Get-NetAdapter -Name "vEthernet (WSL)","vEthernet (Default Switch)" `
 # http://77.222.181.11:8080/mjpg/video.mjpg
 New-NetFirewallRule -DisplayName "Allow Docker Backend Out" -Direction Outbound `
   -Program "C:\Program Files\Docker\Docker\com.docker.backend.exe" -Action Allow -Profile Any -ErrorAction SilentlyContinue
+
+Get-NetFirewallProfile | Select-Object Name, DefaultInboundAction, DefaultOutboundAction
+
+$ifNames = ($aliases | Select-Object -ExpandProperty Name)
+New-NetFirewallRule `
+  -DisplayName "Docker/WSL Outbound Allow (All)" `
+  -Direction Outbound -Action Allow -Protocol Any `
+  -InterfaceAlias $ifNames -Profile Any `
+  -ErrorAction SilentlyContinue
